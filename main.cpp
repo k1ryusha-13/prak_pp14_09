cb737189c30083ce652b9946cdc2f8fe89917e66
kirusha-13 <kirillukashevich2007@gmail.com>
2026-09-17 23:03:47 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <chrono>
+#include <iostream>
+#include <cstddef>
+#include <vector>
+#include <future>
+
+namespace lukashevich
+{
+  class Clicker
+  {
+    public:
+      Clicker():
+        start_(std::chrono::high_resolution_clock::now())
+      {}
+      double millisec() const
+      {
+        using std::chrono::high_resolution_clock;
+        using std::chrono::duration_cast;
+        using std::chrono::milliseconds;
+        auto t = high_resolution_clock::now();
+        return duration_cast< milliseconds >(t - start_).count();
+      }
+    private:
+      std::chrono::time_point< std::chrono::steady_clock > start_;
+  };
+}
+
+using data_t = std::vector< unsigned long long >;
+using value_t = data_t::value_type;
+
+int main() {
+  constexpr size_t size{1'000'000'000};
+  double init{0}, total{0};
+  value_t sum {0};
+  {
+    mtt::Clicker cl;
+    data_t values (size, 1);
+    init = cl.millisec();
+    for (size_t i = 0; i < values.size(); ++i) {
+    sum += values [i];
+    }
+  total = cl.millisec();
+  }
+}
